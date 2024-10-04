51a71640c425cdd3d099547e6028c972488dd731
uma214 <naniuma01@gmail.com>
2024-10-04 01:10:40 -0400

    Add files via upload

---
+++ even.java
@@ -0,0 +1,14 @@
+class even
+{
+public static void main( String[]args)
+{
+Scanner reader=new Scanner(System.in);
+System.out.println("enter a number");
+if(num%2==0)
+{
+System.out.println(num + "is even");
+else
+System.out.println(num + "is odd");
+}
+}
+}
